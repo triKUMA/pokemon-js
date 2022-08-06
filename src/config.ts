3955f9576d4a7653e348405cd6194561ff71654c
tri/KUMA <trikuma@hotmail.com>
2022-08-06 17:20:30 +0800

    Extracted config object from config into index

--- src/config.ts
+++
@@ -1,7 +0,0 @@
-const config = {
-  width: 320,
-  height: 180,
-  margin: 12,
-};
-
-export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
-import config from "./config";
 import { initialiseCanvas, renderImage } from "./canvas";
 
-function main() {
-  const ctx = initialiseCanvas(config.width, config.height, config.margin);
+const config = {
+  canvas: {
+    width: 320,
+    height: 180,
+    margin: 12,
+  },
+  level: {
+    imgSrc: "./img/Pellet Town.png",
+  },
+};
 
-  renderImage(ctx, "./img/Pellet Town.png");
+function main() {
+  const ctx = initialiseCanvas(
+    config.canvas.width,
+    config.canvas.height,
+    config.canvas.margin
+  );
+
+  renderImage(ctx, config.level.imgSrc, -150, -100);
 }
 
 main();
