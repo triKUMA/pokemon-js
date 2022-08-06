ed0c2f8da2851b11882d81dddac58c6919afa108
tri/KUMA <trikuma@hotmail.com>
2022-08-06 21:29:26 +0800

    Sprites can now be drawn as a cropped part of their full image

--- src/sprite.ts
+++ src/sprite.ts
@@ -29,4 +29,29 @@ export const drawSprite = (sprite: Sprite) => {
   sprite.ctx.drawImage(sprite.image, sprite.position.x, sprite.position.y);
 };
 
+interface CropDetails {
+  position: Position;
+  width: number;
+  height: number;
+}
+
+export const drawSpriteCropped = (sprite: Sprite, cropDetails: CropDetails) => {
+  sprite.ctx.drawImage(
+    sprite.image,
+    cropDetails.position.x,
+    cropDetails.position.y,
+    cropDetails.width,
+    cropDetails.height,
+    sprite.position.x,
+    sprite.position.y,
+    cropDetails.width,
+    cropDetails.height
+  );
+};
+
+const drawOrigin = (sprite: Sprite) => {
+  sprite.ctx.fillStyle = "black";
+  sprite.ctx.fillRect(sprite.position.x, sprite.position.y, 1, 1);
+};
+
 export default Sprite;
