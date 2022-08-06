8f607bcb202a65b531454da5f58ee5dd84b35ba5
tri/KUMA <trikuma@hotmail.com>
2022-08-06 21:29:45 +0800

    Basic player movement has now been added

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { initialiseCanvas } from "./canvas";
-import Sprite, { drawSprite } from "./sprite";
+import keys, { Keys } from "./input";
+import Sprite, { drawSprite, drawSpriteCropped } from "./sprite";
 
 const config = {
   canvas: {
@@ -20,6 +21,24 @@ const config = {
   },
 };
 
+const animate = (level: Sprite, player: Sprite, keys: Keys) => {
+  window.requestAnimationFrame(() => animate(level, player, keys));
+  drawSprite(level);
+  drawSpriteCropped(player, {
+    position: { x: 0, y: 0 },
+    width: 12,
+    height: 17,
+  });
+
+  const xVelocity = +keys.a.pressed - +keys.d.pressed;
+  const yVelocity = +keys.w.pressed - +keys.s.pressed;
+
+  level.position = {
+    x: level.position.x + xVelocity,
+    y: level.position.y + yVelocity,
+  };
+};
+
 async function main() {
   const ctx = initialiseCanvas(
     config.canvas.width,
@@ -28,16 +47,15 @@ async function main() {
   );
 
   const level = await Sprite.load(ctx, config.level.imgSrc, {
-    x: -175,
-    y: -125,
+    x: -181,
+    y: -130,
   });
-  const player = await Sprite.load(ctx, config.player.animationSrc.up, {
-    x: 0,
-    y: 0,
+  const player = await Sprite.load(ctx, config.player.animationSrc.down, {
+    x: config.canvas.width / 2,
+    y: config.canvas.height / 2,
   });
 
-  drawSprite(level);
-  drawSprite(player);
+  animate(level, player, keys);
 }
 
 main();
